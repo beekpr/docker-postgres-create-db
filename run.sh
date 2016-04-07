db0c785de7863543aa568e7dffaf13b51835e39c
Filip Wieladek <filip@beekeeper.ch>
2016-04-07 06:39:57 +0200

    Fix SQL syntax

--- run.sh
+++ run.sh
@@ -3,12 +3,23 @@
 databaseName=${PSQL_NEW_DB_NAME}
 password=${PSQL_NEW_DB_PASSWORD}
 
+if [[ -z "$databaseName" ]]; then
+  echo "missing PSQL_NEW_DB_NAME env variable"
+  exit 2
+fi
+
+if [[ -z "$password" ]]; then
+  echo "missing PSQL_NEW_DB_PASSWORD env variable"
+  exit 2
+fi
+
 cat > create.sql << EOF
+
 CREATE ROLE ${databaseName} with LOGIN PASSWORD '${password}';
-GRANT ${databaseName} TO master
+GRANT ${databaseName} TO ${PGUSER};
 
 CREATE DATABASE ${databaseName} with OWNER ${databaseName} ENCODING 'UTF8';
-REVOKE ${databaseName} FROM master
+REVOKE ${databaseName} FROM ${PGUSER};
 EOF
 
 exec psql -f create.sql
